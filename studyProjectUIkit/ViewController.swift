f09b0723da95b77b82f05cc914c0de5818e444cb
Evgenii Kirillov <477504@gmail.com>
2025-03-23 18:23:36 +0400

    Initial Commit

---
+++ studyProjectUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studyProjectUIkit
+//
+//  Created by Евгений on 23.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
